6c06e01baa1110c131f37d6f33f3e41b68e2e652
Tiago Gomes <tidgomes@gmail.com>
2026-06-27 17:33:21 -0300

    refactor the CithubActivityCLI by creating the fetchGihubActiviy displayActivity methods

--- src/GithubActivityCLI.java
+++ src/GithubActivityCLI.java
@@ -1,3 +1,8 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,16 +16,27 @@ public class GithubActivityCLI {
             System.out.println("Usage: java GithubActivityCLI <username>");
             return;
         }
+        GithubActivityCLI cli = new GithubActivityCLI();
+        cli.fetchGithubActivity(args[0]);
+    }
 
-        String GITHUB_API_URL = "https://api.github.com/users/" + args[0] + "/events";
+    private void fetchGithubActivity(String username) {
+        // query parameters would be something like: https://api.github.com/users/<USERNAME>/events?per_page=<INTEGER>";
+        String GITHUB_API_URL = "https://api.github.com/users/" + username + "/events";
 
         HttpClient client = HttpClient.newHttpClient();
         try {
             HttpRequest request = HttpRequest.newBuilder().uri(new URI(GITHUB_API_URL)).header("Accept", "application/vnd.github+json").GET().build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+            if (response.statusCode() == 404) {
+                System.out.println("User not found. Please check the username.");
+                return;
+            }
             if (response.statusCode() == 200) {
-                System.out.println(response);
+                JsonParser parser = new JsonParser();
+                JsonArray jsonArray = parser.parseString(response.body()).getAsJsonArray();
+                displayActivity(jsonArray);
             } else {
                 System.out.println("Error: " + response.statusCode());
             }
@@ -31,6 +47,41 @@ public class GithubActivityCLI {
         } catch (InterruptedException interruptedException) {
             Thread.currentThread().interrupt();
             interruptedException.printStackTrace();
+        }
+    }
+
+    private void displayActivity(JsonArray events) {
+        for (JsonElement element : events) {
+            JsonObject event = element.getAsJsonObject();
+            String type = event.get("type").getAsString();
+            String action;
+
+            switch (type) {
+                case "PushEvent":
+                    int commitCount = event.get("payload").getAsJsonObject().get("commits").getAsJsonArray().size();
+                    action = "Pushed " + commitCount + " commit(s) to " + event.get("repo").getAsJsonObject().get("name");
+                    break;
+                case "IssuesEvent":
+                    action = event.get("payload").getAsJsonObject().get("action").getAsString().toUpperCase().charAt(0)
+                            + event.get("payload").getAsJsonObject().get("action").getAsString()
+                            + " an issue in ${event.repo.name}"; // is this correct? Aren't template string different in java?
+                    break;
+                case "WatchEvent": // isn't this the same as the one below?
+                    action = "Starred " + event.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                case "ForkEvent":
+                    action = "Forked " + event.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                case "CreateEvent":
+                    action = "Created " + event.get("payload").getAsJsonObject().get("name").getAsString()
+                            + " in " + event.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                default:
+                    action = event.get("type").getAsString().replace("Event", "")
+                            + " in " + event.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+            }
+            System.out.println(action);
         }
     }
 }
